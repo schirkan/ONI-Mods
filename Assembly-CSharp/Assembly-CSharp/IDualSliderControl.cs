c32b0eadc7e1c8331b733e138da56b2ea189adb0
Martin Pietschmann <schirkan@googlemail.com>
2020-11-02 13:51:45 +0100

    remove decompiled sources

--- Assembly-CSharp/Assembly-CSharp/IDualSliderControl.cs
+++
@@ -1,9 +0,0 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: IDualSliderControl
-// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: A8D8CAFB-CD16-4CDA-9F47-7D36796BFC75
-// Assembly location: H:\Steam\steamapps\common\OxygenNotIncluded\OxygenNotIncluded_Data\Managed\Assembly-CSharp.dll
-
-public interface IDualSliderControl : ISliderControl
-{
-}
